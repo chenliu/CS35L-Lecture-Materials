f16c8aaa791cbe8904103dfaa1b7e804633b65a5
Chen Liu <chenliu@cs.ucla.edu>
2011-11-06 00:52:03 -0700

    edit example1.c

--- Lecture-12/example1.c
+++ Lecture-12/example1.c
@@ -1,10 +1,11 @@
-void function(int a, int b, int c) {
+int function(int a, int b, int c) {
     int buffer1[2];
     int buffer2[4];
     buffer1[0] = 0;
     buffer1[1] = a;
     buffer2[0] = b;
     buffer2[3] = c;
+    return 0;
 }
 
 int main() {
